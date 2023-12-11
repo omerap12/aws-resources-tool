7643900a7b1601e385293c67c64db5dfe98a25c4
Omer <omerap12@gmail.com>
2023-12-11 22:13:14 +0200

    Used printing

--- main.go
+++ main.go
@@ -2,30 +2,26 @@ package main
 
 import (
 	"aws-resource-inv-tool/ec2"
-	"aws-resource-inv-tool/sts"
 	"fmt"
 )
 
 func main() {
 
 	// User validor checker
-	sts_output, err := sts.CheckCredetianls()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("Connected as: ", *sts_output.Arn)
-
-	// instances, err := ec2.GetAllIntances()
+	// sts_output, err := sts.CheckCredetianls()
 	// if err != nil {
-	// 	fmt.Printf(err.Error())
+	// 	fmt.Println(err)
 	// 	return
 	// }
-	// fmt.Println(instances)
-	instances, err := ec2.GetInstancesByType("t2.micro")
+	// fmt.Println("Connected as: ", *sts_output.Arn)
+	query := make(ec2.RequestArgs)
+	// query["instance-type"] = []string{"t2.micro"}
+
+	instances, err := ec2.GetInstances(&query)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	fmt.Println(instances)
+
+	ec2.PrintInstances(instances)
 
 }
